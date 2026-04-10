bfc2be8fa72b000c29aa57c01ba239b479b41e34
Enhle-Lulu <st10471705@myemeris.edu.za>
2026-04-10 12:01:29 +0200

    first commit
    pushing main, login and JUnit test

---
+++ src/main/java/com/mycompany/chatmessenger/Login.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.chatmessenger;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Login {
+    
+    //variables to store user data
+    private String username;
+    private String password;
+    private String phoneNumber;
+    
+    public void setUsername(String username){
+        this.username = username;
+    }
+    
+    public void setPassword(String password){
+        this.password = password;
+    }
+    
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+    
+    public boolean checkUsername(){
+        return username.contains("_")&& username.length()<=5;
+    }
+    
+    public boolean checkPasswordComplexity(){
+        return password.length()>=8 &&
+                password.matches(".*[A-Z].*")&&
+                password.matches(".*[0-9].*")&&
+                password.matches(".*[!@#$%^&*()].*");
+    }
+    
+    public boolean checkCellPhoneNumber(){
+        return phoneNumber.startsWith("+27")&& phoneNumber.length()== 12;
+    }
+    
+    
+}
