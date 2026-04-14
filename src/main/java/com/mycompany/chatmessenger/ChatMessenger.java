0f4abfcd67c385ec6c4f1730e9fa3c9cbc58e766
Enhle-Lulu <st10471705@myemeris.edu.za>
2026-04-14 15:02:16 +0200

    11th Commit
    Scanner addition on Main

--- src/main/java/com/mycompany/chatmessenger/ChatMessenger.java
+++ src/main/java/com/mycompany/chatmessenger/ChatMessenger.java
@@ -8,20 +8,49 @@ package com.mycompany.chatmessenger;
  *
  * @author lab_services_student
  */
+
+import java.util.Scanner;
+
 public class ChatMessenger {
 
     public static void main(String[] args) {
         
+        Scanner input = new Scanner(System.in);
         Login user = new Login();
         
-        user.setUsername("kyl_1");
-        user.setPassword("Ch&&sec@ke99!");
-        user.setCellPhone("+27838968976");
+        System.out.println("Enter first name");
+        user.setFirstName(input.nextLine());
+        
+        System.out.println("Enter last name");
+        user.setLastName(input.nextLine());
+        
+        System.out.println("Enter username");
+        user.setUsername(input.nextLine());
+        
+        System.out.println("Enter password");
+        user.setPassword(input.nextLine());
+        
+        System.out.println("Enter cell phone number (+27):");
+        user.setCellPhone(input.nextLine());
         
         System.out.println(user.registerUser());
         
-        boolean login = user.loginUser("kyl_1", "Ch&&sec@ke99!");
+        System.out.println("\n Login Section");
         
-        System.out.println(user.returnLoginStatus(login));
+        System.out.println("Enter username");
+        String loginUsername = input.nextLine();
+        
+        System.out.println("Enter username");
+        String loginPassword = input.nextLine();
+        
+        //user.setUsername("kyl_1");
+        //user.setPassword("Ch&&sec@ke99!");
+        //user.setCellPhone("+27838968976");
+        
+        //System.out.println(user.registerUser());
+        
+        boolean loginStatus = user.loginUser(loginUsername,loginPassword);
+        
+        System.out.println(user.returnLoginStatus(loginStatus));
     }
 }
